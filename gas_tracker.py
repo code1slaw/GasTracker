02f39e971c6de283fd465f5bd7f03d9855f032b8
code1slaw <bov88@yandex.ru>
2022-08-11 12:22:33 +0300

    small updates

--- gas_tracker.py
+++ gas_tracker.py
@@ -5,23 +5,20 @@ import time
 
 network.connect('arbitrum-main')
 
-graph_width = 60*60*24 # seconds
-points_x_count = 24*60
+graph_width = timedelta(days=1).total_seconds()
+points_x_count = 24*60*60
 sleep_time = graph_width/points_x_count
+
+plt.title(network.show_active() + " gas history for last 24 hours")
+plt.ylabel("Gas price")
 
 x = []
 y = []
 
-plt.title(network.show_active())
-plt.ylabel("Gas price")
-
-# inital graph point
-x.append(int(datetime.now().timestamp()))
-y.append(float(web3.fromWei(web3.eth.gasPrice, 'gwei')))
-
-while True:
-    plt.clt()
-    plt.cld()
+while True:    
+    plt.clear_terminal()
+    plt.clear_data()    
+    #plt.plot_size(width=plt.terminal_width(), height=plt.terminal_height())    
 
     date_time = int(datetime.now().timestamp())
     gas_price = float(web3.fromWei(web3.eth.gasPrice, 'gwei'))
@@ -38,12 +35,12 @@ while True:
     plt.xticks(xticks, xlabels)
 
     # setting xlabel to current day
-    plt.xlabel("Date " + datetime.fromtimestamp(x[0]).strftime("%d/%m/%Y"))
+    plt.xlabel("Starting date " + datetime.fromtimestamp(x[0]).strftime("%d/%m/%Y"))
     # setting x axis limit to first element timestamp + 1 day
     plt.xlim(x[0], x[0] + timedelta(days=1).total_seconds())
     # setting y axis limit to 110% of max value
     plt.ylim(0, float(max(y))*1.1) 
     plt.plot(x, y)        
     plt.show()
-
+    
     time.sleep(sleep_time)
